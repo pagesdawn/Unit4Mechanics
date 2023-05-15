ec087dbccc706d7ba5087de57acf55daa12be3e6
Christian Familia <cfamilia0913@gmail.com>
2023-05-15 14:09:30 -0400

    4.3.2

--- Unit4Mechanics/Assets/Scripts/PlayerController.cs
+++ Unit4Mechanics/Assets/Scripts/PlayerController.cs
@@ -6,9 +6,11 @@ public class PlayerController : MonoBehaviour
 {
 
     public float speed = 5.0f;
+    private float powerUpStrength = 15.0f;
     private Rigidbody playerRb;
     private GameObject focalPoint;
     public bool hasPowerup;
+    public GameObject powerupIndicator;
 
     // Start is called before the first frame update
     void Start()
@@ -23,6 +25,8 @@ public class PlayerController : MonoBehaviour
         float forwardInput = Input.GetAxis("Vertical");
 
         playerRb.AddForce(focalPoint.transform.forward * forwardInput * speed);
+
+        powerupIndicator.transform.position = transform.position + new Vector3(0, -0.5f, 0);
     }
 
     private void OnTriggerEnter(Collider other)
@@ -30,14 +34,27 @@ public class PlayerController : MonoBehaviour
         if (other.CompareTag("Powerup"))
         {
             hasPowerup = true;
+            powerupIndicator.gameObject.SetActive(true);
             Destroy(other.gameObject);
+            StartCoroutine(PowerupCountdownRoutine());
         }
     }
 
-    private void OnCollisionEnter(Collision collision)
+    IEnumerator PowerupCountdownRoutine()
+    {
+        yield return new WaitForSeconds(7);
+        hasPowerup = false;
+        powerupIndicator.gameObject.SetActive(false);
+    }
+
+    private async void OnCollisionEnter(Collision collision)
     {
         if (collision.gameObject.CompareTag("Enemy") && hasPowerup)
         {
+            Rigidbody enemyRigidbody = collision.gameObject.GetComponent<Rigidbody>();
+            Vector3 awayFromPlayer = collision.gameObject.transform.position - transform.position;
+
+            enemyRigidbody.AddForce(awayFromPlayer * powerUpStrength, ForceMode.Impulse);
             Debug.Log("Collided with: " + collision.gameObject.name + "with powerup set to " + hasPowerup);
         }
     }
